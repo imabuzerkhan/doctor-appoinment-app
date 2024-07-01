e7cd3741ab7b80bef466299690ce28069bd42d96
imabuzerkhan <abuzerk41@gmail.com>
2024-07-01 21:13:22 +0545

    Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "@/_components/Hero";
 import CategorySearch from "@/_components/CategorySearch";
+import Footer from "@/_components/Footer";
 
 export default function Home() {
   return (
    <>
    <Hero></Hero>
    <CategorySearch/>
+   <Footer></Footer>
    </>
   );
 }
