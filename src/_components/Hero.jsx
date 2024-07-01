33d6249b3286ac03fc2cdcff19c67812bc08a780
imabuzerkhan <abuzerk41@gmail.com>
2024-07-01 20:32:28 +0545

    hero section completed

---
+++ src/_components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+const Hero = () => {
+  return (
+    <section>
+  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+        <Image
+          alt="docto.img"
+          src="/happy-doctors.jpg"
+          width={600}
+          height={600}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div className="lg:py-24">
+        <h2 className="text-3xl font-bold sm:text-4xl">Book Your Appointment</h2>
+
+        <p className="mt-4 text-gray-600">
+        Are you in need of medical attention? Our expert doctors are here to help. Schedule your appointment today for a comprehensive consultation. 
+         
+        </p>
+
+       <Button className="mt-6" >Explore more</Button>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Hero from "@/_components/Hero";
 
 export default function Home() {
   return (
    <>
+   <Hero></Hero>
    </>
   );
 }
