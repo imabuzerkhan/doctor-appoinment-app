c2f62b35c09e7a33e3a101c2c909d706e3f314ba
imabuzerkhan <abuzerk41@gmail.com>
2024-08-04 19:32:57 +0545

    adding calendar

--- src/app/Details/_components/Booking.jsx
+++ src/app/Details/_components/Booking.jsx
@@ -6,6 +6,8 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { Calendar } from "@/components/ui/calendar"
+
 import { NotebookPen } from "lucide-react"
 
 
@@ -17,11 +19,16 @@ const Booking = () => {
   <SheetTrigger className="flex item-center gap-1 font-bold px-2 py-2 text-white bg-[#272E3F] dark:text-black dark:bg-white rounded-lg  " ><NotebookPen/> Booking Appoinment </SheetTrigger>
   <SheetContent className="overflow-auto" >
     <SheetHeader>
-      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetTitle>Booking Appoinment </SheetTitle>
       <SheetDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+      Get your time and appoinment date from here
       </SheetDescription>
+      <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border"
+  />
     </SheetHeader>
   </SheetContent>
 </Sheet>
